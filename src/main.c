8f7ae73da98e2619cfd6194afb4122252d0b4028
spm1102 <dovietdung_hsgs19@hus.edu.vn>
2024-05-04 19:48:54 +0700

    [CREATE] create folder

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef struct books {
+    char title[30];
+    char author[30];
+    int year;
+    char publisher[30];
+    int price_in;
+    int price_out;
+} books;
+
+typedef struct book {
+    int pages;
+    char size[10];
+} book;
+
+typedef struct musics { 
+    char title[30];
+    char author[30];
+    int year;
+    char publisher[30];
+    int price_in;
+    int price_out;
+} musics;
+
+typedef struct music {
+    int time_run;
+    int capacity;
+} music;
+
+void listing(int* ptr_list);
+
+int main(){
+
+    return 0;
+}
+
+void listing(int* ptr_list){
+    for(int )
+    printf("")
+}
